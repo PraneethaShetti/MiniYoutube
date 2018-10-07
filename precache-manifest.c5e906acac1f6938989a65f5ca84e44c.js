4f6539638b3dbb0d044a013bfd0e2b638a8b6a1b
PraneethaShetti <pshetti062@gmail.com>
2018-10-07 12:48:24 -0700

    Updates

---
+++ precache-manifest.c5e906acac1f6938989a65f5ca84e44c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "51883e1cbccb83910925",
+    "url": "/MiniYoutube/static/js/runtime~main.51883e1c.js"
+  },
+  {
+    "revision": "89e2877effed63cc83f7",
+    "url": "/MiniYoutube/static/js/main.89e2877e.chunk.js"
+  },
+  {
+    "revision": "2bd23f18149fb357596b",
+    "url": "/MiniYoutube/static/js/1.2bd23f18.chunk.js"
+  },
+  {
+    "revision": "89e2877effed63cc83f7",
+    "url": "/MiniYoutube/static/css/main.49d698f0.chunk.css"
+  },
+  {
+    "revision": "7cad86b1e1dca89fe0df2be25529fc8e",
+    "url": "/MiniYoutube/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
+
+importScripts(
+  "/MiniYoutube/precache-manifest.c5e906acac1f6938989a65f5ca84e44c.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/MiniYoutube/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
